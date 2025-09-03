338b1673a7a2b99bb0cdc83ddee606e715abd4a4
gusmmm <jgustavomartins@gmail.com>
2025-09-03 14:17:32 +0100

    Refactor test_create_doente to use session fixture and validate database entry

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,27 @@
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 from the_wicker_man.app import app
+from the_wicker_man.models import table_registry
 
 
 @pytest.fixture
 def client():
     return TestClient(app)
+
+
+@pytest.fixture
+def session():
+    engine = create_engine('sqlite:///:memory:')
+    table_registry.metadata.create_all(engine)
+    TestingSessionLocal = sessionmaker(autocommit=False,
+                                       autoflush=False,
+                                       bind=engine)
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        table_registry.metadata.drop_all(engine)
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,9 +1,11 @@
+from sqlalchemy import select
+
 from the_wicker_man.models import Doente
 
 NUMERO_PROCESSO = 12345
 
 
-def test_create_doente(client):
+def test_create_doente(session):
     doente_data = Doente(
         numero_processo=NUMERO_PROCESSO,
         nome="João Silva",
@@ -11,13 +13,15 @@ def test_create_doente(client):
         sexo="M",
         morada="Rua Exemplo, 123"
     )
+    session.add(doente_data)
+    session.commit()
 
-    assert doente_data.numero_processo == NUMERO_PROCESSO
-    assert doente_data.nome == "João Silva"
-    assert doente_data.data_nascimento == "1980-01-01"
-    assert doente_data.sexo == "M"
-    assert doente_data.morada == "Rua Exemplo, 123"
-    # ID should be None before being set by the database
-    assert doente_data.id is None
-    # created_at should be None before being
-    assert doente_data.created_at is None
+    doente_in_db = session.scalar(
+        select(Doente).where(Doente.numero_processo == NUMERO_PROCESSO)
+    )
+    assert doente_in_db is not None
+    assert doente_in_db.nome == "João Silva"
+    assert doente_in_db.numero_processo == NUMERO_PROCESSO
+    assert doente_in_db.sexo == "M"
+    assert doente_in_db.morada == "Rua Exemplo, 123"
+    assert doente_in_db.data_nascimento == "1980-01-01"
