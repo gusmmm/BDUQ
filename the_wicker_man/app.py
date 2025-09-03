e09b91fb1bb0b32f0bed4a409e3d07d80c8d2e55
gusmmm <jgustavomartins@gmail.com>
2025-09-03 09:17:14 +0100

    Enhance FastAPI application: add Message schema, update root endpoint response model, and modify run command to specify port

--- the_wicker_man/app.py
+++ the_wicker_man/app.py
@@ -1,8 +1,12 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from the_wicker_man.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', response_model=Message, status_code=HTTPStatus.OK)
 def read_root():
     return {'message': 'Olá Mundo!'}
